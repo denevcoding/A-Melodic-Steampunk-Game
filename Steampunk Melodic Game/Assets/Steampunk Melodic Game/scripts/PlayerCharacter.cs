de9af991f076f7800dd1e54a0679871572307695
Jose Barrios <estaravia@hotmail.com>
2021-03-03 20:07:26 -0500

    Player movement test

--- Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
+++ Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+    public float force = 1;
+    
     
     // Start is called before the first frame update
     void Start()
     {
+        
         //rigidBody = this.gameObject.AddComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * force;
     }
 }
