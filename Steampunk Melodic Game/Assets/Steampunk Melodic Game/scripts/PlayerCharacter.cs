f144fecaa0346f7af9fdb70dde6d380efa1a6929
kevin Velez <kevin.velez@live.u-tad.com>
2021-03-09 23:31:20 -0500

    Moving By Velocity

--- Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
+++ Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    private Rigidbody2D rigidBody;
+    public Rigidbody2D rigidBody;
     public float force = 1;
-    
-    
+    public float speed = 3.0f;
+    Vector3 moveDirection = Vector2.zero;
+
+    Vector3 inputVector = Vector3.zero;
+
+    void Awake() 
+    {
+        rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
-    {
+    {        
         
-        //rigidBody = this.gameObject.AddComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * force;
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * 5f, rigidBody.velocity.y, 0);
+
+        //moveDirection.x = Input.GetAxis("Horizontal");
+        //moveDirection.y = rigidBody.velocity.y;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(Vector3.up * 500.0f);
+        }
+
+       
+        //transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * force;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = inputVector;
+        //rigidBody.MovePosition();
+
+
     }
 }
