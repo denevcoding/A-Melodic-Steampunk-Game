bd7d5845b7ca4ae9623c2a5d2a30ac5c5d678176
Jose Barrios <estaravia@hotmail.com>
2021-03-10 18:50:02 -0500

    Changes

--- Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
+++ Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
@@ -6,19 +6,41 @@ public class PlayerCharacter : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
     public float force = 1;
-    
+    private Animator animatorPlayer;
+    private float scaleX;
+    public float gravity = 9.8f;
+    public float jumpSpeed = 20;
+    CharacterController controller;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        scaleX = transform.localScale.x;
+        animatorPlayer = this.GetComponent<Animator>();
+        controller = this.GetComponent<CharacterController>();
         //rigidBody = this.gameObject.AddComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * force;
+    
+        float XInput = Input.GetAxis("Horizontal");
+        
+
+        animatorPlayer.SetFloat("XVelocity", Mathf.Abs(XInput));
+        //Debug.Log("movement" + YInput);
+        transform.position += new Vector3(XInput, 0, 0) * Time.deltaTime * force;
+        if (XInput < 0)
+        {
+            transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+
+        }
     }
 }
