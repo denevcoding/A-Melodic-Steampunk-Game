ce3a5535367d09d921c2f1a15dee2ddfc8e2420a
kevin Velez <kevin.velez@live.u-tad.com>
2021-03-09 23:04:19 -0500

    HandoutUYpdated

---
+++ Steampunk Melodic Game/Assets/Handout/Events/Scripts/Events/EventDispatcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventDispatcher : MonoBehaviour {
+
+	public delegate void MyEventAction();
+	public event MyEventAction OnEvent;
+    
+    void OnGUI() {
+        if(GUI.Button(new Rect(Screen.width / 2 - 50, 5, 200, 30), "Click For an Event")) {
+            if(OnEvent != null) OnEvent();
+        }
+    }
+
+    void OnMouseDown() {
+		if(OnEvent != null)
+			OnEvent();
+	}
+}
