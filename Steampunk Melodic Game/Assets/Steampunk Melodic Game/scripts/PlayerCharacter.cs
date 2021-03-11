1730c63166b4d77675195b58ae9d7057d4cb5386
Jose Barrios <estaravia@hotmail.com>
2021-03-10 19:53:32 -0500

    Movement test

--- Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
+++ Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
@@ -8,12 +8,13 @@ public class PlayerCharacter : MonoBehaviour
     public float force = 1;
     public float speed = 3.0f;
     Vector3 moveDirection = Vector2.zero;
-
+    Animator animatorPlayer;
     Vector3 inputVector = Vector3.zero;
 
     void Awake() 
     {
         rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+        animatorPlayer = this.gameObject.GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -29,12 +30,20 @@ public class PlayerCharacter : MonoBehaviour
 
         //moveDirection.x = Input.GetAxis("Horizontal");
         //moveDirection.y = rigidBody.velocity.y;
-
+        animatorPlayer.SetFloat("XVelocity", Mathf.Abs(inputVector.x));
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigidBody.AddForce(Vector3.up * 500.0f);
         }
+        if (inputVector.x > 0)
+        {
 
+        }
+        else if (inputVector.x < 0)
+        {
+            Vector3 rotationVector = new Vector3(0,-180,0);
+            //transform.rotation = rotationVector;
+        }
        
         //transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * force;
     }
