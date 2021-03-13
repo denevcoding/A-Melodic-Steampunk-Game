37c25dcf97885d6796d702f34c4ba420bf7193a4
kevin Velez <kevin.velez@live.u-tad.com>
2021-03-13 11:08:12 -0500

    Design: Player Jumping

--- Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
+++ Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/PlayerCharacter.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerCharacter : MonoBehaviour
 {
-    public Rigidbody2D rigidBody;
-    public float force = 1;
-    public float speed = 3.0f;
-    Vector3 moveDirection = Vector2.zero;
+
+    private Rigidbody2D rigidBody;
+    private BoxCollider2D boxCollider;
     Animator animatorPlayer;
+
+    [Tooltip("The amount of velocity to move the character")]
+    public float speed = 3.0f;    
     Vector3 inputVector = Vector3.zero;
+    bool isRight = true;
+
+    public bool isGrounded;
+
+    [SerializeField]
+    private LayerMask platformMask;
+
+
 
     void Awake() 
     {
-        rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
-        animatorPlayer = this.gameObject.GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        animatorPlayer = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -27,32 +39,67 @@ public class PlayerCharacter : MonoBehaviour
     void Update()
     {
         inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * 5f, rigidBody.velocity.y, 0);
-
-        //moveDirection.x = Input.GetAxis("Horizontal");
-        //moveDirection.y = rigidBody.velocity.y;
+ 
         animatorPlayer.SetFloat("XVelocity", Mathf.Abs(inputVector.x));
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidBody.AddForce(Vector3.up * 500.0f);
-        }
-        if (inputVector.x > 0)
-        {
 
-        }
-        else if (inputVector.x < 0)
-        {
-            Vector3 rotationVector = new Vector3(0,-180,0);
-            //transform.rotation = rotationVector;
-        }
+
+        Jump();
        
-        //transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * force;
+
+        FlipCharacter();       
     }
 
     private void FixedUpdate()
     {
-        rigidBody.velocity = inputVector;
-        //rigidBody.MovePosition();
+        rigidBody.velocity = inputVector;        
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(Vector3.up * 500.0f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        float extraDistance = 0.1f;
+
+        Vector2 origin = boxCollider.bounds.center;
+
+        RaycastHit2D rayCastHit = Physics2D.Raycast(origin, Vector2.down, boxCollider.bounds.extents.y + extraDistance, platformMask);
+        Color rayColor;
+
+        if (rayCastHit.collider != null)
+        {
+            rayColor = Color.blue;
+        }
+        else
+        {
+            rayColor = Color.red;           
+        }
+
+        Debug.DrawRay(origin, Vector2.down * (boxCollider.bounds.extents.y + extraDistance), rayColor);
+        Debug.Log(rayCastHit.collider);        
+
+        return rayCastHit.collider != null;
+    }
 
 
+    private void FlipCharacter()
+    {
+        if (inputVector.x < 0 && isRight || inputVector.x > 0 && !isRight)
+        {
+            isRight = !isRight;
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
+    }
+
+
+    //event Called from collition by physic Manager 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Chocó " + collision.gameObject.name);
     }
 }
