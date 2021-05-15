2ac67afdcc83983e83df9a4d27bcc86c0264e8f1
Jose Barrios <estaravia@hotmail.com>
2021-05-15 10:12:59 -0500

    Map test

--- Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/Managers/GameLogic.cs
+++ Steampunk Melodic Game/Assets/Steampunk Melodic Game/scripts/Managers/GameLogic.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
-
-
+using Cinemachine;
 
 public class GameLogic : MyGenericSingleton<GameLogic>
 {
@@ -12,6 +10,9 @@ public class GameLogic : MyGenericSingleton<GameLogic>
 
     public Action<float> platformQueue;
 
+    public Camera mainCamera;
+    public CinemachineVirtualCamera cvc1;
+    public CinemachineVirtualCamera cvc2;
 
     public GameObject playerPrefab;
     public Transform playerSpawn;
@@ -27,7 +28,11 @@ public class GameLogic : MyGenericSingleton<GameLogic>
     public void Start()
     {
        ninjaPlayer = Instantiate(playerPrefab, playerSpawn.position, Quaternion.identity);         
-       ninjaPlayer.GetComponent<PlayerCharacter>().InitPlayer(playerSpawn.position);  
+       ninjaPlayer.GetComponent<PlayerCharacter>().InitPlayer(playerSpawn.position);
+        cvc1.gameObject.SetActive(false);
+        cvc2.gameObject.SetActive(true);
+        CameraHandler cvc2handler = cvc2.GetComponent<CameraHandler>();
+        cvc2handler.AttachFollowTarget(GameLogic.singleton.ninjaPlayer.transform);
     }
 
 }//closes GameLogic
